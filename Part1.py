937f5b9a696779705329410734cabeb4b56d0c32
Aaron33 <aaron33@tamu.edu>
2024-10-06 23:21:07 -0500

    Mostly done with part 1

--- Part1.py
+++ Part1.py
@@ -1,6 +1,13 @@
 from sklearn import tree
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.gaussian_process import GaussianProcessClassifier
+from sklearn.metrics import accuracy_score
 
 clf = tree.DecisionTreeClassifier()
+clf2 = KNeighborsClassifier()
+clf3 = GaussianNB()
+clf4 = GaussianProcessClassifier()
 
 # CHALLENGE - create 3 more classifiers...
 # 1
@@ -18,9 +25,19 @@ Y = ['male', 'male', 'female', 'female', 'male', 'male', 'female', 'female',
 
 # CHALLENGE - ...and train them on our data
 clf = clf.fit(X, Y)
+clf2 = clf2.fit(X, Y)
+clf3 = clf3.fit(X, Y)
+clf4 = clf4.fit(X, Y)
 
 prediction = clf.predict([[190, 70, 43]])
+prediction2 = clf2.predict([[190, 70, 43]])
+prediction3 = clf3.predict([[190, 70, 43]])
+prediction4 = clf4.predict([[190, 70, 43]])
 
 # CHALLENGE compare their reusults and print the best one!
+print(accuracy_score(prediction, prediction2))
 
 print(prediction)
+print(prediction2)
+print(prediction3)
+print(prediction4)
